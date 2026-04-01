f4b48434e31ebd474cfc56e3d2765850674592fb
utsxvv <utssavvpatel@gmail.com>
2026-04-01 20:56:11 +0530

    save uploaded image

--- PharmaLens/views.py
+++ PharmaLens/views.py
@@ -1,4 +1,1 @@
 from django.shortcuts import render
-
-def home(request):
-    return render(request, "home.html", {"title": "Home"})
--- PharmaLensApp/views.py
+++ PharmaLensApp/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from django.core.files.storage import FileSystemStorage
 
-# Create your views here.
+def home(request):
+    if request.method == "POST" and request.FILES.get("prescription"):
+        file = request.FILES["prescription"]
+
+        fs = FileSystemStorage()
+        filename = fs.save(file.name, file)
+
+        file_url = fs.url(filename)
+        # Here you would typically process the file and extract information
+        # For now, we just pass the file URL to the template
+        return render(request, "home.html", {
+                "file_url": file_url, 
+                "uploaded": True
+            })
+    return render(request, "home.html", {"title": "Home"})
